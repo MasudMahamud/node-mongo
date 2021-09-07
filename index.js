65faf1f46d71dd4b861607e7670bcc5cc94ba732
MasudMahamud <mahamud.al.msdud@gmail.com>
2021-09-08 02:21:21 +0600

    show user

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Thanks for using node.js')
+const users = ['Deny', 'Rahim Uddin', 'Karim Uddin', 'Susmita', 'Suborna', 'Samiha', 'Sakib']
+
+// app.get('/', (req, res) => {
+//     res.send('Thanks for using node.js')
+// })
+
+app.get('/users/:id', (req, res) =>{
+   
+    const userId = req.params.id;
+    const name = users[userId];
+    res.send({userId, name})
 })
 
-app.listen(5000, ()=>console.log('listening'))
+app.listen(5000, ()=>console.log('listening to port 5000'))
